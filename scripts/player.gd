325cdb8810b092eb55876c903ce1e6c16dfadc04
thasu1 <zkpatel0912@gmail.com>
2025-07-17 20:27:45 -0500

    added a first person camera and balaced the movements

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 @export_group("Movement")
-@export var thrust_force: float = 10.0
-@export var boost_force: float = 25.0
+@export var thrust_force: float = 7.0
+@export var boost_force: float = 10.0
 @export var boost_cooldown: float = 2.0
 
 @export_group("Camera")
@@ -10,19 +10,26 @@ extends CharacterBody3D
 
 var can_boost = true
 var _camera_input_direction := Vector2.ZERO
+var is_first_person: bool = true
 
 @onready var _camera_pivot: Node3D = %camerapivot
+@onready var _first_person_camera: Camera3D = %FirstPersonCamera
+@onready var _third_person_camera: Camera3D = %ThirdPersonCamera
 @onready var _timer: Timer = $Timer
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	_timer.timeout.connect(_on_Timer_timeout)
+	_first_person_camera.current = true
+	_third_person_camera.current = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event.is_action_pressed("toggle_view"):
+		toggle_camera()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
@@ -32,11 +39,11 @@ func _physics_process(delta: float) -> void:
 	handle_camera_look(delta)
 	handle_movement(delta)
 	handle_boost()
-	move_and_slide()  # No argument needed in Godot 4
+	move_and_slide()  # Godot 4 uses internal velocity
 
 func handle_camera_look(delta: float) -> void:
-	_camera_pivot.rotation.x += _camera_input_direction.y * delta
-	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, -PI / 2.5, PI / 3.0)
+	_camera_pivot.rotation.x -= _camera_input_direction.y * delta  # Inverted Y
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, -PI / 3.0, PI / 6.0)
 	rotation.y -= _camera_input_direction.x * delta
 	_camera_input_direction = Vector2.ZERO
 
@@ -66,6 +73,11 @@ func handle_boost():
 		velocity += forward * boost_force
 		can_boost = false
 		_timer.start()
+
+func toggle_camera():
+	is_first_person = !is_first_person
+	_first_person_camera.current = is_first_person
+	_third_person_camera.current = !is_first_person
 
 func _on_Timer_timeout():
 	can_boost = true
